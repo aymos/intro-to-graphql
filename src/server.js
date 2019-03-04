925f8d0cc4222774d8b2ae5f15d1d4254ca5de2c
somya <somya@indeed.com>
2019-03-03 17:52:56 -0800

    Add basic types and resolvers and play around

--- src/server.js
+++ src/server.js
@@ -9,25 +9,40 @@ import user from './types/user/user.resolvers'
 
 const types = ['product', 'coupon', 'user']
 
-export const start = async () => {
-  const rootSchema = `
+export const start = async() => {
+    const rootSchema = `
+    type Cat {
+      name:String
+      age:Int!
+    }
+
+    type Query {
+      myCat: Cat
+    }
+
     schema {
       query: Query
     }
   `
-  const schemaTypes = await Promise.all(types.map(loadTypeSchema))
+    const schemaTypes = await Promise.all(types.map(loadTypeSchema))
 
-  const server = new ApolloServer({
-    typeDefs: [rootSchema],
-    resolvers: {},
-    context({ req }) {
-      // use the authenticate function from utils to auth req, its Async!
-      return { user: null }
-    }
-  })
+    const server = new ApolloServer({
+        typeDefs: [rootSchema],
+        resolvers: {
+            Query: {
+                myCat() {
+                    return { name: 'Pup', age: 1 }
+                }
+            }
+        },
+        context({ req }) {
+            // use the authenticate function from utils to auth req, its Async!
+            return { user: null }
+        }
+    })
 
-  await connect(config.dbUrl)
-  const { url } = await server.listen({ port: config.port })
+    await connect(config.dbUrl)
+    const { url } = await server.listen({ port: config.port })
 
-  console.log(`GQL server ready at ${url}`)
+    console.log(`GQL server ready at ${url}`)
 }
