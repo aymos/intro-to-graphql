2b11524f00a11cbbd0030a6e6385baddf7c01564
somya <somya@indeed.com>
2019-03-03 18:59:20 -0800

    All tests for lesson 3 except resolveProduct

--- src/types/product/product.resolvers.js
+++ src/types/product/product.resolvers.js
@@ -4,15 +4,37 @@ import { AuthenticationError } from 'apollo-server'
 import mongoose from 'mongoose'
 
 const productsTypeMatcher = {
-  GAMING_PC: 'GamingPc',
-  BIKE: 'Bike',
-  DRONE: 'Drone'
+    GAMING_PC: 'GamingPc',
+    BIKE: 'Bike',
+    DRONE: 'Drone'
+}
+
+const products = () => {
+    return Product.find()
+}
+
+const product = (_, args) => {
+    return Product.findById(args.id)
+}
+
+const newProduct = (_, args, ctx) => {
+    // use this fake ID for createdBy for now until we talk auth
+    const createdBy = mongoose.Types.ObjectId()
+    return Product.create({...args.input, createdBy })
+}
+
+const updateProduct = (_, args, ctx) => {
+    return Product.findByIdAndUpdate(args.id, args.input, { new: 'true' })
+}
+
+const removeProduct = (_, args) => {
+    return Product.findByIdAndRemove(args.id);
 }
 
 export default {
-  Query: {},
-  Mutation: {},
-  Product: {
-    __resolveType(product) {}
-  }
+    Query: { products, product },
+    Mutation: { newProduct, updateProduct, removeProduct },
+    Product: {
+        __resolveType(product) {}
+    }
 }
